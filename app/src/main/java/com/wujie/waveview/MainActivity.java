d7e946881c0a29a6a4f91fc85d7aa4b7d68fb0ff
wujie <1072878103@qq.com>
2017-11-13 10:28:39 +0800

    1 更新基本代码

--- app/src/main/java/com/wujie/waveview/MainActivity.java
+++ app/src/main/java/com/wujie/waveview/MainActivity.java
@@ -1,13 +1,38 @@
 package com.wujie.waveview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+
+import com.wujie.waveview.view.WaveView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private SeekBar seekBar;
+    private WaveView waveView;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
+        waveView = (WaveView) findViewById(R.id.wave_view);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                waveView.setProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
